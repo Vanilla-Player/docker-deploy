8ae2e7724e578c459147cbe93a18cc9e64a2c559
Luca Braccani <lucabraccani@Lucas-MacBook-Pro.local>
2023-12-19 13:34:53 -0300

    Iniciando repo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+
 # docker-deploy
 Practicando el deploy de aplicaciones
---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Adios desde docker, por segunda vezss!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
